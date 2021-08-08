5da323830652a0fa5f065d33207cac26a2a1cb64
Sander van Dragt <sander@vandragt.com>
2021-08-08 10:26:22 +0100

    Briefcased

--- fafi/src/fafi/app.py
+++ fafi/src/fafi/app.py
@@ -1,9 +1,49 @@
+"""
+Bookmarking application
+"""
+import toga
+from toga.style import Pack
+from toga.style.pack import COLUMN, ROW
 from contextlib import closing
 
-import appdata
 import datetime
-import db
 import newspaper
+
+from fafi import db
+
+def callback(sender):
+    print("Command activated")
+
+
+class Fafi(toga.App):
+
+    def startup(self):
+        """
+        Construct and show the Toga application.
+
+        Usually, you would add your application to a main content box.
+        We then create a main window (with a name matching the app), and
+        show the main window.
+        """
+        main_box = toga.Box()
+
+        cmd_index = toga.Command(
+            callback,
+            label='Index bookmarks',
+            tooltip='Tells you when it has been activated',
+            shortcut='i',
+            icon='icons/pretty.png',
+            group=toga.Group.EDIT,
+        )
+        self.commands.add(cmd_index)
+
+        self.main_window = toga.MainWindow(title=self.formal_name)
+        self.main_window.content = main_box
+        self.main_window.show()
+
+
+def main():
+    return Fafi()
 
 
 def index_site(conn, row, verbose):
