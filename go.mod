b11f9d202cdf2fa3f9e3a9651ce7633b64cbf5f6
Sander van Dragt <sander@vandragt.com>
2023-10-09 14:30:18 +0100

    Basic Sqlite Access

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module fafi2
 
 go 1.18
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.17 // indirect
+)
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -8,6 +10,7 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var tpl = template.Must(template.ParseFiles("pub/index.html"))
@@ -26,7 +29,9 @@ func getSearchQuery(w http.ResponseWriter, r *http.Request) string {
 
 	params := u.Query()
 	searchQuery := params.Get("q")
-	log.Println("Search: ", searchQuery)
+	if searchQuery != "" {
+		log.Println("Search: ", searchQuery)
+	}
 	return searchQuery
 }
 
@@ -45,11 +50,26 @@ func startServer(port string) {
 }
 
 func main() {
+	// Environment variables
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file!")
+		log.Fatal(err)
 	}
-	port := os.Getenv("FAFI_PORT")
 
+	// Database access
+	db, err := sql.Open("sqlite3", "file:data/fafi.sqlite3")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	var version string
+	err = db.QueryRow("SELECT SQLITE_VERSION()").Scan(&version)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("SQLite3 version", version)
+
+	// Server start
+	port := os.Getenv("FAFI_PORT")
 	startServer(port)
 }
